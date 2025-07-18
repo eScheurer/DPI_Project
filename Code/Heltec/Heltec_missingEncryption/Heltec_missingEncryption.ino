86bd3a8e58783dc3321bb634cf1ed48d7d978070
eScheurer <enya.scheurer@quickline.ch>
2025-07-18 12:47:27 +0200

    adds encription draft, commit for peer programming

--- Code/Heltec/Heltec_missingEncryption/Heltec_missingEncryption.ino
+++ Code/Heltec/Heltec_missingEncryption/Heltec_missingEncryption.ino
@@ -1,6 +1,9 @@
 // Configuration ---------------------------------------------------------
 // OLED
 #include "HT_SSD1306Wire.h"
+#include "mbedtls/aes.h" // encription 
+#include "mbedtls/base64.h"
+#include "esp_system.h" // for esp_random()
 
 static SSD1306Wire display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED);
 
@@ -78,6 +81,90 @@ const uint16_t BUF_SIZE = 256;
 char ioBuffer[BUF_SIZE];      
 // Configuration ---------------------------------------------------------
 
+// SECURITY --------------------------------------------------------------
+// key for testing
+const uint8_t aesKey[16] = {
+  0x12, 0x34, 0x56, 0x78,
+  0x9A, 0xBC, 0xDE, 0xF0,
+  0x11, 0x22, 0x33, 0x44,
+  0x55, 0x66, 0x77, 0x88
+};
+
+// const uint8_t aesKey[16] = {0x50, 0xd8, 0x58, 0xe0,
+                            // 0x60, 0x41, 0x8a, 0xaf, 
+                            // 0x7f, 0x42, 0xc2, 0x57, 
+                            // 0xa9, 0xe8, 0xcc, 0xac, };
+
+String encrypt(String rawText){
+  mbedtls_aes_context aes;
+  uint8_t iv[16]; // Initialisierungsvektor
+  size_t length = rawText.length();
+  size_t paddedLength = ((length/16) +1) *16; // aes works with 16bit blocks, need padding to fit it
+  uint8_t input[paddedLength];
+  uint8_t output[paddedLength];
+  memset(input, 0, paddedLength); // "Zero-Padding"
+  memcpy(input, rawText.c_str(), length);
+
+  esp_fill_random(iv, 16);
+  mbedtls_aes_init(&aes);
+  mbedtls_aes_setkey_enc(&aes, aesKey, 128); //128 Bit == 16 Byte
+  mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, paddedLength, iv, input, output);
+
+  uint8_t encoded[16 + paddedLength];
+  memcpy(encoded, iv, 16);
+  memcpy(encoded + 16, output, paddedLength);
+
+  size_t outputLength;
+  char base64[512]; //TODO: size?????????????????????????????????
+  mbedtls_base64_encode((unsigned char*)base64, sizeof(base64), &outputLength, encoded, 16+paddedLength); // for transmission
+  base64[outputLength] = '\0'; // make it correct String
+
+  mbedtls_aes_free(&aes);
+  
+  return String((char*)base64);
+}
+
+String decrypt(String encriptedText) {
+  mbedtls_aes_context aes;
+  uint8_t decoded[512];
+  size_t decodedLength;
+  int result = mbedtls_base64_decode(decoded, sizeof(decoded), &decodedLength,(const uint8_t*)encriptedText.c_str(), encriptedText.length());
+
+  if (result != 0) {
+    return "Error: Decoding went wrong!";
+  }
+  if (decodedLength < 16) {
+    return "Error: message too short, likely damaged";
+  }
+  uint8_t iv[16];
+  memcpy(iv, decoded, 16);
+  size_t rawLength = decodedLength -16;
+  uint8_t* rawText = decoded + 16;
+
+  //uint8_t decrypted[rawLength];
+  uint8_t* decrypted = (uint8_t*)malloc(rawLength);
+  if (!decrypted){
+    return "Error: malloc failed";
+  } 
+
+  mbedtls_aes_init(&aes);
+  mbedtls_aes_setkey_dec(&aes, aesKey, 128);
+  mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, rawLength, iv, rawText, decrypted);
+  mbedtls_aes_free(&aes);
+
+  if (result != 0) {
+    free(decrypted);
+    return "Error: decryption failed";
+  }
+
+  while (decrypted[rawLength -1] == 0 && rawLength > 0) {
+    rawLength -= 1; // remove the zeros padding from the end
+  }
+  String output = String((char*)decrypted, rawLength);
+  free(decrypted);
+  return output;
+}
+
 // WEBSTUFF --------------------------------------------------------------
 // Sends HTML to client
 void handleRoot() {
@@ -132,19 +219,22 @@ void OnRxDone(uint8_t* payload, uint16_t size, int16_t rssi, int8_t snr) {
         payload[size] = '\0';
         String msg = String((char*)payload);
         Serial.println("Empfangen: " + msg);
-        onReceiveMessage(msg);
+        String decrypted = decrypt(msg);
+        Serial.println("Decrypted: " + decrypted);
+        onReceiveMessage(decrypted);
     }
     Radio.Rx(0);
 }
 
 void sendLoRaMessage(const String &msg) {
-    size_t len = msg.length();
-    if (len > BUF_SIZE - 1) {
+    Serial.println("Raw: " + msg);
+    String encrypted = encrypt(msg);
+    size_t len = encrypted.length();    if (len > BUF_SIZE - 1) {
         len = BUF_SIZE - 1;
     }
-    msg.toCharArray(ioBuffer, len + 1);
+    encrypted.toCharArray(ioBuffer, len + 1);
     Radio.Send((uint8_t*)ioBuffer, len);
-    Serial.println("Sent: " + msg);
+    Serial.println("Sent: " + encrypted);
 }
 // LORASTUFF --------------------------------------------------------------
 
