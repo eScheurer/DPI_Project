10205f2ec6aedf2ea844be196f3b41e819175e9b
Luis <luis.wenger2@gmail.com>
2025-07-18 11:56:32 +0200

    Remove message count check

--- Code/Heltec/Heltec_HTML_OLED_LORA_CRDTv3luis/Heltec_HTML_OLED_LORA_CRDTv3luis.ino
+++ Code/Heltec/Heltec_HTML_OLED_LORA_CRDTv3luis/Heltec_HTML_OLED_LORA_CRDTv3luis.ino
@@ -239,10 +239,10 @@ void handleIDList(String list) {
   std::map<String, IDElement, MacLess> remotelist;
   int start = 0;
 
-  // Parse remote ID list into a map
+  // Getting all the remotes and putting them into remoteList
   while (start < list.length()) {
     int sep = list.indexOf('|', start);
-    if (sep == -1) sep = list.length();
+    if (sep == -1) sep = list.length();  // Handle last entry without trailing '|'
 
     String entry = list.substring(start, sep);
     int c1 = entry.indexOf('_');
@@ -258,28 +258,21 @@ void handleIDList(String list) {
     start = sep + 1;
   }
 
-  // Go through remote entries and compare to local
+  // Send missing messages based on remote state
   for (const auto& kv : remotelist) {
     const String& rmac = kv.first;
-    int rcount = kv.second.numberOfMessages;
+    int remoteId = kv.second.id;
 
-    int localCount = 0;
-    for (const auto& msg : crdtList) {
-      if (msg.sender == rmac) localCount++;
-    }
-
-    // If remote has fewer messages we resend all messages
-    if (rcount < localCount) {
-      for (const auto& msg : crdtList) {
-        if (msg.sender == rmac) {
-          sendLoRaMessage("MSG|" + msg.id + "|" + msg.content);
-          delay(300);
-        }
+    for (const auto& local : crdtList) {
+      // Match mac address to our version && we have a greater time then the remote for some esp
+      if (local.sender == rmac && local.time > remoteId) {
+        // Sending of that missing message
+        sendLoRaMessage("MSG|" + local.id + "|" + local.content);
+        delay(300);
       }
     }
   }
 }
-
 
 // Received message needs to be decoded
 void onReceiveMessage(String &received) {
